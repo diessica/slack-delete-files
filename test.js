29bb7a92635f3ab3277d8e635c5a4b141a672665
Diéssica <diessicode@gmail.com>
2018-09-01 11:14:45 +0200

    Remove test for feedback message

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 const test = require("ava")
 const { filterFiles } = require("./")
 
-test("[filterFiles] throws message for no results", t => {
-  const errorMessage = t.throws(() => filterFiles({})([]))
-  t.is(errorMessage, "There are no files to be deleted.")
-})
-
 test("[filterFiles] doesn't include pinned files if inquired", t => {
   const fakeFiles = [
     {
